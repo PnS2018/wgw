dab9cf024b923d0fd8c4522993f4657d194d0678
Eric Marty <mrdederi@gmail.com>
2018-04-12 15:29:58 +0200

    DataProcessor MVP

---
+++ data/DataProcessor.py
@@ -0,0 +1,16 @@
+import cv2
+import os
+
+IMAGE_SIZE = 64
+
+
+class DataProcessor:
+
+    def __init__(self):
+        self.image_size = 64
+        self.train_waldo = 'train_waldo'
+        self.train_notwaldo = 'train_notwaldo'
+        self.test_waldo = 'test_waldo'
+        self.test_notwaldo = 'test_notwaldo'
+
+    def
